434c73e059cca629e0a45505d08d64af69a27ced
BioX-NKU <104627413+BioX-NKU@users.noreply.github.com>
2022-11-30 10:35:10 +0800

    Update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ import sys
 import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath(__file__+'../../../..'))
 
-import epiaster
+import aster
 # -- Project information -----------------------------------------------------
 
 project = 'ASTER'
